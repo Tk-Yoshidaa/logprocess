95f1a3aa350805e4972a6027f229f113ecbd4c0c
Takumi Yoshida <107405763+Tk-Yoshidaa@users.noreply.github.com>
2025-01-25 17:37:06 +0900

    Update logprocess.sh

--- logprocess.sh
+++ logprocess.sh
@@ -4,31 +4,34 @@ logprocess(){
     local interval=$3
     local devnull=/dev/null
 
-    local command=$(ps -p $pid -o comm --no-headers)
-
-    if [ -z "$pid" ]; then
-        echo "Usage: logprocess PID OUTPUT_PATH [INTERVAL(sec) default: 20]"
+    if [ -z "$pid" ] || [ -z $output ]; then
+        echo "usage: logprocess PID OUTPUT_PATH [INTERVAL(sec) default: 60]"
+        return
     fi
 
     if [ -z "$interval" ]; then
-        local interval=20
+        local interval=60
     fi
 
+    local command=$(ps -p $pid -o comm --no-headers)
     echo logprocess start logging. pid: $pid, command: $command, output: $output
 
-    local cmd=\"ps -p $pid -o %cpu,%mem --no-headers\"
+    local cmd="ps -p $pid -o %cpu,%mem --no-headers"
 
     nohup bash -c "
+    export PATH=\$PATH:/bin:/usr/bin;
     echo pid:$pid/command:$command > $output
     echo date time %mem %cpu >> $output
     while true; do
-        local time=$(date '+%Y-%m-%d %H:%M:%S')
-        local value=$($cmd)
-        if [ -z \"$value\" ]; then
+        time=\$(date '+%Y-%m-%d %H:%M:%S')
+        value=\$($cmd)
+        if [ -z \"\$value\" ]; then
             echo logprocess missing process. pid: $pid 
-            exit 1
+            break
         fi
-        echo $time $value >> $output
+        echo time:\$time 
+        echo value:\$value
+        echo \$time \$value >> $output
         sleep $interval
     done
     " > $devnull &
